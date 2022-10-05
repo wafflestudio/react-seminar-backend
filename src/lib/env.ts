29ba2ae0524cbbd2a4470b681808dc07cae84f79
joongwon <winterdawnlight@snu.ac.kr>
2022-10-05 20:40:44 +0900

    add error message

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,15 +2,21 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const _MYSQL_USERNAME = process.env.MYSQL_USERNAME;
-const _MYSQL_PASSWORD = process.env.MYSQL_PASSWORD;
-const _MYSQL_DATABASE = process.env.MYSQL_DATABASE;
-const _JWT_SECRET = process.env.JWT_SECRET;
+const KEYS = [
+  "MYSQL_USERNAME",
+  "MYSQL_PASSWORD",
+  "MYSQL_DATABASE",
+  "JWT_SECRET"
+] as const;
+type KeyType = (typeof KEYS)[number];
 
-if (!(_MYSQL_USERNAME && _MYSQL_PASSWORD && _MYSQL_DATABASE && _JWT_SECRET))
+const KVPS = KEYS.map(key => [key, process.env[key]] as [KeyType, string | undefined]);
+if (KVPS.some(([,v]) => v === undefined)) {
+  console.error(`please set environment variables: ${KEYS.toString()}`);
   process.exit(1);
+}
+const OBJ = Object.fromEntries(KVPS) as { [k in KeyType]: string };
 
-export const MYSQL_USERNAME = _MYSQL_USERNAME;
-export const MYSQL_PASSWORD = _MYSQL_PASSWORD;
-export const MYSQL_DATABASE = _MYSQL_DATABASE;
-export const JWT_SECRET = _JWT_SECRET;
+export const {
+ MYSQL_PASSWORD, MYSQL_USERNAME, MYSQL_DATABASE, JWT_SECRET
+} = OBJ;
