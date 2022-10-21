93f801622cb7960e8e21f8550626a1f5df713a88
joongwon <winterdawnlight@snu.ac.kr>
2022-10-21 21:10:31 +0900

    fix deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ yarn build
 echo "$ scp ./build remote:~/project/"
 scp -r -i "$KEY_FILE" ./build "$REMOTE_USER@$REMOTE_HOST:$REMOTE_PROJECT_ROOT/"
 echo "$ ssh remote update"
-ssh -i "$KEY_FILE" "$REMOTE_USER@$REMOTE_HOST" "cd \"$REMOTE_PROJECT_ROOT\" && git pull && sh ./script/update.sh"
+ssh -i "$KEY_FILE" "$REMOTE_USER@$REMOTE_HOST" "cd \"$REMOTE_PROJECT_ROOT\" && git pull && sh ./scripts/update.sh"
