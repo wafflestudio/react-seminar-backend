d1fe9dd20a1bf00a201a975255a8b003ea33c898
joongwon <winterdawnlight@snu.ac.kr>
2022-11-11 11:57:12 +0900

    Revert "add debug log; ignore the error"
    This reverts commit 3adf1060b43fa253e4ebdc56aea2264c4e6d619b.

--- src/auth/model.ts
+++ src/auth/model.ts
@@ -2,17 +2,12 @@ import { PrismaClient } from "@prisma/client";
 import { refreshTokenInvalid } from "../lib/errors";
 import { REFRESH_TOKEN_EXPIRATION } from "../lib/tokens";
 import { selectOne } from "../lib/utils";
-import { FastifyBaseLogger } from "fastify";
 
 export class RefreshTokenModel {
   conn: PrismaClient;
-  log: FastifyBaseLogger;
-
-  constructor(conn: PrismaClient, log: FastifyBaseLogger) {
+  constructor(conn: PrismaClient) {
     this.conn = conn;
-    this.log = log;
   }
-
   async insert(refresh_token: string, owner_id: number): Promise<void> {
     await this.conn.refreshToken.create({
       data: {
@@ -35,18 +30,14 @@ export class RefreshTokenModel {
   }
 
   async remove(refresh_token: string): Promise<void> {
-    const _list = await this.conn.refreshToken.findMany({
-      where: { token: refresh_token },
-      select: { id: true },
-    });
-    const tokenEntity = selectOne(_list);
+    const tokenEntity = selectOne(
+      await this.conn.refreshToken.findMany({
+        where: { token: refresh_token },
+        select: { id: true },
+      })
+    );
     if (!tokenEntity) throw refreshTokenInvalid();
-    await this.conn.refreshToken
-      .delete({ where: { id: tokenEntity.id } })
-      .catch((reason) => {
-        this.log.error(reason, "something went wrong");
-        this.log.error(_list, "this was the tokens we've got");
-      });
+    await this.conn.refreshToken.delete({ where: { id: tokenEntity.id } });
   }
 
   async purge(): Promise<void> {
