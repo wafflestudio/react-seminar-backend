db5667bc4c5260fdfea8fed6040e18805e503a19
joongwon <freleefty@gmail.com>
2023-12-02 18:59:19 +0900

    migrate on start

--- scripts/init-data.ts
+++ scripts/init-data.ts
@@ -5,6 +5,11 @@ import "../src/lib/env";
 
 async function main() {
   const conn = new PrismaClient();
+  const n = await conn.owner.findMany().then((owners) => owners.length);
+  if (n > 0) {
+    console.log("Already initialized");
+    return;
+  }
   const ownerModel = new OwnerModel(conn);
   await ownerModel.insertMany(
     students.map((student) => ({
